2642ea8d6d8281b9ef6975bb49ec14a6c4977907
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-09-16 11:54:36 +0100

    Added superhero class

---
+++ oop-2025-b-0916-12345/Models/SuperHero.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_2025_b_0916_12345.Models
+{
+    public class SuperHero
+    {
+        public string Name { get; set; } = String.Empty;
+        public string Alias { get; set; } = String.Empty;
+    }
+}
--- oop-2025-b-0916-12345/Program.cs
+++ oop-2025-b-0916-12345/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using oop_2025_b_0916_12345.Models;
+
+//string Greeting = "Hello";
+//string MyName = "John";
+//int Age = 63;
+//Console.WriteLine($"{Greeting},{MyName}! You are {Age} years old");
+
+//string SuperHero1_Name = "Tony Stark";
+//string SuperHero1_Alias = "Iron Man";
+//string SuperHero2_Name = "Peter Parker";
+//string SuperHero2_Alias = "SpiderMan";
+
+//Console.WriteLine($"{SuperHero1_Alias},{SuperHero1_Name}");
+//Console.WriteLine($"{SuperHero2_Alias},{SuperHero2_Name}");
+
+SuperHero superHero1 = new();
+superHero1.Name = "Tony Stark";
+superHero1.Alias = "Iron Man";
+
+SuperHero superHero2 = new();
+superHero2.Name = "Peter Parker";
+superHero2.Alias = "SpiderMan";
+
+
+SuperHero superHero3 = new() { 
+    Alias = "Deadpool", 
+    Name = "Wade Wilson" 
+};
+
+
+Console.WriteLine($"{superHero1.Alias},{superHero1.Name}");
+Console.WriteLine($"{superHero2.Alias},{superHero2.Name}");
+Console.WriteLine($"{superHero3.Alias},{superHero3.Name}");
